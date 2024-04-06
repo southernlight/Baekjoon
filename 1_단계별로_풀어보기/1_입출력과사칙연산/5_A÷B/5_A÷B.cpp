4ff69a1195e703fe3a31d06a1328f0bce9180882
Kim Nam Hoon <southernlight3816@gmail.com>
2024-04-06 23:32:02 +0900

    사칙연산 추가

--- 1_단계별로_풀어보기/1_입출력과사칙연산/5_A÷B/5_A÷B.cpp
+++ 1_단계별로_풀어보기/1_입출력과사칙연산/5_A÷B/5_A÷B.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int b;
+    double a;
+    double b;
     cin >> a >> b;
-    cout << (float)a / (float)b;
+    cout << a / b;
     return 0;
 }
