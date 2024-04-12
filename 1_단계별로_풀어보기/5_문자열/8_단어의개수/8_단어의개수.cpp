59437a3c931cb201e08f67b0f37fc6d405ca2b1e
Kim Nam Hoon <southernlight3816@gmail.com>
2024-04-12 20:51:35 +0900

    1152

--- 1_단계별로_풀어보기/5_문자열/8_단어의개수/8_단어의개수.cpp
+++ 1_단계별로_풀어보기/5_문자열/8_단어의개수/8_단어의개수.cpp
@@ -2,16 +2,24 @@
 using namespace std;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
     char str[1000000];
     cin.getline(str, 1000000);
+    int count = 0;
     int i = 0;
     while (1)
     {
         if (str[i] == '\0')
             break;
-
+        if (str[i] == ' ')
+        {
+            if (i != 0 && str[i + 1] != '\0')
+                count++;
+        }
         i++;
     }
+    cout << count + 1;
 
     return 0;
 }
