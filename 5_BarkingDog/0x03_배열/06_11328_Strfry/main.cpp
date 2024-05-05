cf2c005970214cb45a98bdb3981e8c6bbd6fbca4
Kim Nam Hoon <southernlight3816@gmail.com>
2024-05-05 23:51:35 +0900

    commit

--- 5_BarkingDog/0x03_배열/06_11328_Strfry/main.cpp
+++ 5_BarkingDog/0x03_배열/06_11328_Strfry/main.cpp
@@ -4,22 +4,24 @@ using namespace std;
 int N;
 bool occur1[26];
 bool occur2[26];
+bool possible;
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cin >> N;
     char first[1001];
     char second[1001];
     for (int i = 0; i < N; i++)
     {
+
+        possible = true;
         cin >> first >> second;
         int first_len = strlen(first);
         int second_len = strlen(second);
         if (first_len != second_len)
-        {
-            cout << "Impossible\n";
-        }
-
+            possible = false;
         else
         {
             for (int j = 0; j < first_len; j++)
@@ -31,10 +33,15 @@ int main()
             {
                 if (occur1[i] != occur2[i])
                 {
-                    ;
+                    possible = false;
+                    break;
                 }
             }
         }
+        if (possible)
+            cout << "Possible\n";
+        else
+            cout << "Impossible\n";
     }
 
     return 0;
